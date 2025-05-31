a61bc718c11c193dd9ecb21b64c07b4ec8a26827
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2025-05-31 20:45:13 +0700

    Update thuviencoban.h

--- thuviencoban.h
+++ thuviencoban.h
@@ -1,26 +1,14 @@
-/* 1 VÀI THƯ VIỆN CƠ BẢN TRONG BITS/STDC++.H */
+/* 1 VÀI THƯ VIỆN CƠ BẢN DÙNG TRONG CHƯƠNG TRÌNH */
 
-// Thư viện nhập xuất cơ bản
 #include <iostream>
-
-// Thư viện string
-#include <string>
-#include <cstring>
-
-// Thư viện thao tác với vector, set, map...
-#include <vector>
-#include <set>
-#include <map>
-
-// Thư viện thuật toán
-#include <algorithm>
-
-// Thư viện toán học
-#include <cmath>
-#include <cstdlib>
-
-// Thư viện iomanip (định dạng in sau dấu chấm)
+#include <fstream>
 #include <iomanip>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <conio.h>
+#include <unistd.h>
 
 using namespace std;
 
